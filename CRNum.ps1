6f1a1e44e276d4168cae512cf1e84355d2a9d8c8
BorraVisalakshi <55823754+BorraVisalakshi@users.noreply.github.com>
2021-05-08 20:17:39 +0530

    Update CRNum.ps1

--- CRNum.ps1
+++ CRNum.ps1
@@ -1,6 +1,6 @@
 
 $web_client = new-object System.Net.Webclient
-$web_client.Credentials = New-Object System.Net.NetworkCredential(admin, 12345678)
+$web_client.Credentials = New-Object System.Net.NetworkCredential("admin", "12345678")
 $response_info = $web_client.DownloadString("https://dev94240.service-now.com/change_request.do?JSONv2&sysparm_query=number%CHG0030005")
 $response_json = ConvertFrom-Json $response_info
 If($response_json.records[0].approval -ne 'approved')
